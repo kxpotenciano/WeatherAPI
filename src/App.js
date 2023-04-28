2d4495aa827d2b752ef27128892d98a5d5fff026
kxpotenciano <86062808+kxpotenciano@users.noreply.github.com>
2023-04-28 17:26:53 +0800

    with tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const api = {
+    key: "4f8e795dcd6dbf7b9f5276bff095ffc1",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+
+  const [queue, setQueue] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${queue}&appid=${api.key}&units=metric`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQueue('');
+          console.log(result);
+        });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main className='min-h-screen p-[25px]'>
+        <div className="content-center items-center place-items-center justify-items-center ">
+          <input 
+            type="text"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  
+            placeholder="City"
+            onChange={e => setQueue(e.target.value)}
+            value={queue}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="text-center">
+            <div className="relative inline-block text-white text-[50px] font-black mx-auto my-[30] px-[25] py-[15]">{weather.name}, {weather.sys.country}</div>
+          </div>
+
+          <div className="text-center">
+            <div className="relative inline-block text-white text-[50px] font-black mx-auto my-[30] px-[25px] py-[15]">Temperature: {Math.round(weather.main.temp)}°C</div>
+            <div className="relative inline-block text-white text-[50px] font-black mx-auto my-[30] px-[25px] py-[15]">Humidity: {weather.main.humidity}</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
     </div>
   );
 }
-
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.js",
+    "./index.css",
+    "./App.css",
+    "./index.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
